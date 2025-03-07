8e92dc9baddb726687f07e6755513ea5a506a3f7
Your Name <your.email@example.com>
2025-03-07 05:50:31 +0100

    feat(csharp): create Element class

---
+++ PowderToyCSharp/Element.cs
@@ -0,0 +1,13 @@
+using System.Drawing;
+
+namespace PowderToyCSharp
+{
+    public class Element
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public float Density { get; set; }
+        public float Flammability { get; set; }
+        public int State { get; set; } // 0: Solid, 1: Liquid, 2: Gas
+    }
+}
