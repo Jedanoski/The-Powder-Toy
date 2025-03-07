9aa33129592d7440f3b24404ac5a00ef0f4e29bf
Your Name <your.email@example.com>
2025-03-07 05:53:32 +0100

    fix(csharp): initialize Name property in Element class

--- PowderToyCSharp/Element.cs
+++ PowderToyCSharp/Element.cs
@@ -4,7 +4,7 @@ namespace PowderToyCSharp
 {
     public class Element
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public Color Color { get; set; }
         public float Density { get; set; }
         public float Flammability { get; set; }
